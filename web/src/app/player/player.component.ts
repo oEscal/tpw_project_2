5bf1136c186b8143f00bc28c215a1526f8a65505
escaleira <pedroescaleira@hotmail.com>
2019-12-16 10:10:45 +0000

    player page component ready for interface

--- web/src/app/player/player.component.ts
+++ web/src/app/player/player.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Team} from '../entities';
+import {Player, Team} from '../entities';
 import {RestApiService} from '../rest-api.service';
 import {ActivatedRoute} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
@@ -11,5 +11,26 @@ import {HttpErrorResponse} from '@angular/common/http';
 })
 export class PlayerComponent implements OnInit {
 
-  
+  player: Player;
+  player_id: number;
+
+  error_message: string = '';
+
+  constructor(private rest_api_service: RestApiService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {this.player_id = params.id; });
+    this.get_team();
+  }
+
+  get_team(): void {
+    this.rest_api_service.get_player(this.player_id).subscribe(
+      result => this.player = result.data as Player,
+      error => this.handle_error(error));
+  }
+
+  handle_error(error: HttpErrorResponse) {
+    console.log(error);
+    this.error_message = error.error.message;
+  }
 }
