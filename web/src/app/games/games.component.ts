3fd04c0221ccfba5e273274df6852324012ee313
escaleira <pedroescaleira@hotmail.com>
2019-12-16 21:50:09 +0000

    games page completly done

--- web/src/app/games/games.component.ts
+++ web/src/app/games/games.component.ts
@@ -3,6 +3,8 @@ import {Game, Player} from '../entities';
 import {RestApiService} from '../rest-api.service';
 import {ActivatedRoute} from '@angular/router';
 import {HttpErrorResponse} from '@angular/common/http';
+import $ from 'jquery';
+
 
 @Component({
   selector: 'app-games',
@@ -19,6 +21,18 @@ export class GamesComponent implements OnInit {
 
   ngOnInit() {
     this.get_games();
+
+    // jquery code
+    $(document).ready(function () {
+      $(".more_info_btn").click(function () {
+        $(".more_info").hide(1000);
+        $("#more_info_" + this.id).show(1000);
+      });
+
+      $(".less_info_btn").click(function () {
+        $("#more_info_" + this.id).hide(1000);
+      });
+    });
   }
 
   get_games(): void {
