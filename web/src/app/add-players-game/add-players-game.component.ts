cbc52abfed0b2142d950ea7d9e4320fb73e23b30
escaleira <pedroescaleira@hotmail.com>
2019-12-16 22:57:15 +0000

    add players to game component done

--- web/src/app/add-players-game/add-players-game.component.ts
+++ web/src/app/add-players-game/add-players-game.component.ts
@@ -13,39 +13,23 @@ export class AddPlayersGameComponent implements OnInit {
 
   players;
   game_id;
-  teams;
-  new_player;
 
   error_message: string;
   success_message: string;
 
   constructor(private formBuilder: FormBuilder,
               private rest_api_service: RestApiService,
-              private route: ActivatedRoute) {
-    this.new_player = this.formBuilder.group({
-      name: '',
-      birth_date: '',
-      nick: '',
-      position: '',
-      team: ''
-      // photo: File,
-    });
-  }
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {this.game_id = params.id; });
     this.rest_api_service.get_game_team_players(this.game_id).subscribe(
       result => this.players = result.data,
       error => this.handle_error(error));
-
-    // TODO -> posteriormente, poderá adicionar-se um método à rest api só para retornar os nomes das equipas, para não tornar esta chamada tão pesada
-    this.rest_api_service.get_teams().subscribe(
-      result => this.teams = result.data,
-      error => this.handle_error(error));
   }
 
-  add_player(new_player): void {
-    this.rest_api_service.add_player(new_player).subscribe(
+  add_players_game(new_players_game): void {
+    this.rest_api_service.add_players_game(new_players_game, this.game_id).subscribe(
       result => this.success_message = result.message,
       error => this.handle_error(error));
   }
